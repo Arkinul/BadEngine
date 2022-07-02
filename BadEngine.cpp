4386ac38b2b2b9d831dcba8c79750385efdfc1f1
Arkinul <45757532+Arkinul@users.noreply.github.com>
2022-07-02 15:25:57 +0200

    some precomputations and starting with moves

--- BadEngine.cpp
+++ BadEngine.cpp
@@ -4,40 +4,67 @@ using namespace std;
 using ll = long long;
 
 
-static const char None = 0;
-static const char King = 1;
-static const char Pawn = 2;
-static const char Knight = 3;
-static const char Bishop = 4;
-static const char Rook = 5;
-static const char Queen = 6;
+static const int None = 0;
+static const int King = 1;
+static const int Pawn = 2;
+static const int Knight = 3;
+static const int Bishop = 4;
+static const int Rook = 5;
+static const int Queen = 6;
 
-class Position {
+static const int White = 8;
+static const int Black = 16;
+
+static const string startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+
+static const vector<int> dirOffsets = {8,-8,-1,1,7,-7,9,-9};
+static vector<vector<int>> numSquaresToEdge(64,vector<int>(8));
+
+static void precomputeMoveData(){
+    for (int file = 0;file<8;file++){
+        for(int rank = 0;rank<8;rank++){
+            int numUp = 7-rank;
+            int numDown = rank;
+            int numLeft = file;
+            int numRight = 7-file;
+
+            int squareIndex = rank*8+file;
+
+            numSquaresToEdge[squareIndex] = {numUp,numDown,numLeft,numRight,min(numUp,numLeft),min(numDown,numRight),min(numUp,numRight),min(numDown,numLeft)};
+        }
+    }
+}
+
+struct chessMove;
+
+class chessPosition {
 public:
-    vector<char> board;
+    vector<int> board;
     char turn;
     vector<bool> castlingRights;
     int halfmoves;
     int fullmoves;
     bool isInCheck;
+    list<chessMove> pseudoLegalMoves;
+    list<chessMove> legalMoves;
 
-    Position(){
+    chessPosition(){
         for(int i = 0; i<64;i++){
             board.push_back(0);
         }
         turn = 8;
         halfmoves = 0;
-        fullmoves = 0;
+        fullmoves = 1;
         isInCheck = false;
     }
-    explicit Position(string fen){
+    explicit chessPosition(string fen){
         auto pieceFromSymbol = new map<char, char>  {
                 {'k',King}, {'p', Pawn}, {'n',Knight}, {'b',Bishop},{'r',Rook},{'q',Queen}
         };
         string fenboard = fen.substr(0,fen.find_first_of(' '));
         int file = 0,rank = 7;
         for(int i = 0; i<64;i++){
-            board.push_back(0);
+            board.push_back(None);
         }
         for(auto symbol: fenboard){
             if(symbol == '/'){
@@ -45,13 +72,28 @@ public:
                 rank--;
             }else{
                 if(isdigit(symbol)){
-                    file += atoi(&symbol);
+                    file += stoi(&symbol);
                 }else{
-                    //TODO
+                    int pieceColor = isupper(symbol) ? White : Black;
+                    int pieceType = pieceFromSymbol->at(tolower(symbol));
+                    board[rank*8+file] = pieceColor | pieceType;
+                    file++;
                 }
             }
         }
+        turn = 8;
+        halfmoves = 0;
+        fullmoves = 1;
+        isInCheck = false;
+        //TODO: actually use the FEN here too
     }
+
+};
+
+class chessMove{
+public:
+    int startSquare;
+    int endSquare;
 };
 
 //TODO: first the board must work, then other things
@@ -59,6 +101,13 @@ public:
 
 
 int main() {
-    cout << atoi("0") << endl;
+    precomputeMoveData();
+    auto testpos = new chessPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    for(int rank = 7;rank>=0;rank--){
+        for(int file = 0;file<8;file++){
+            cout<<testpos->board[rank*8+file]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
